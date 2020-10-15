ec5e2ca3e648bb6172bfa4d95cbad78e8d20684d
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2020-10-15 22:07:07 +0700

    add service on the same thread

---
+++ app/src/main/java/com/example/myservice/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myservice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_start_service.setOnClickListener(this)
+        btn_start_intent_service.setOnClickListener(this)
+        btn_start_bound_service.setOnClickListener(this)
+        btn_stop_bound_service.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View) {
+        when (p0.id) {
+            R.id.btn_start_service -> {
+                val mStartServiceIntent = Intent(this@MainActivity, MyService::class.java)
+                startService(mStartServiceIntent)
+            }
+
+            R.id.btn_start_intent_service -> {
+                val mStartIntentService = Intent(this@MainActivity, MyIntentService::class.java)
+                mStartIntentService.putExtra(MyIntentService.EXTRA_DURATION, 5000L)
+                startService(mStartIntentService)
+            }
+
+            R.id.btn_start_bound_service -> {
+
+            }
+
+            R.id.btn_stop_bound_service -> {
+
+            }
+        }
+    }
+}
